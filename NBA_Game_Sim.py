b4174477bf2c59bb70c6350e07d85dc5b78f2626
Deane <85832930+dcal1985@users.noreply.github.com>
2022-06-06 16:31:36 -0400

    Update NBA_Game_Sim.py

--- NBA_Game_Sim.py
+++ NBA_Game_Sim.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
 
 # import libraries 
 import pandas as pd
@@ -12,23 +8,12 @@ import matplotlib.pyplot as plt
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 
-
-# In[2]:
-
-
 # access all games from 2021-22 regular season
 games = leaguegamefinder.LeagueGameFinder(season_nullable='2021-22', league_id_nullable='00', 
                                           season_type_nullable = 'Regular Season').get_data_frames()[0]
 
-
-# In[3]:
-
-
 # create new field for points allowed by team
 games['ALLOWED'] = games['PTS']-games['PLUS_MINUS']
-
-
-# In[5]:
 
 
 # select final columns and group by team for mean and standard deviation 
@@ -37,23 +22,14 @@ averages = games_final.groupby(['TEAM_ID', 'TEAM_NAME']).mean()
 stdv = games_final.groupby(['TEAM_ID', 'TEAM_NAME']).std()
 
 
-# In[6]:
-
-
 # rename columns and create final data frame 
 averages.rename(columns = {'PTS':'PTS_AVG', 'ALLOWED':'ALLOWED_AVG'}, inplace = True)
 stdv.rename(columns = {'PTS':'PTS_STDV', 'ALLOWED':'ALLOWED_STDV'}, inplace = True)
 df_final = pd.merge(averages, stdv, on=["TEAM_NAME"])
 
 
-# In[7]:
-
-
 df_final = df_final.reset_index()
 df_final.head()
-
-
-# In[8]:
 
 
 # create game class with inputs for home and away team
@@ -70,9 +46,6 @@ class Game:
         self.away_team_pts_stdv = df_final.PTS_STDV[df_final.TEAM_NAME == away_team].to_numpy()[0]
         self.away_team_allowed_avg = df_final.ALLOWED_AVG[df_final.TEAM_NAME == away_team].to_numpy()[0]
         self.away_team_allowed_stdv = df_final.ALLOWED_STDV[df_final.TEAM_NAME == away_team].to_numpy()[0]  
-
-
-# In[15]:
 
 
 # create simulation function with input for game and number of iterations 
@@ -104,19 +77,11 @@ def Sim(Game, N):
     print(Game.away_team, "average points", round(sum(away_scores)/len(away_scores)))
 
 
-# In[16]:
-
 
 New_Game = Game("Orlando Magic", "Boston Celtics")      
 
 
-# In[17]:
-
-
 Sim(New_Game, 1000)
-
-
-# In[ ]:
 
 
 
